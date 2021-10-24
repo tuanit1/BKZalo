210e1b8a7c4ee79e8bfd2a5342b0d36031d21ae3
tuanit1 <dotuan9655@gmail.com>
2021-10-24 17:27:50 +0700

    zz

---
+++ app/src/main/java/com/example/bkzalo/activitiy/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.bkzalo.activitiy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.bkzalo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        AnhXa();
+    }
+
+    private void AnhXa(){
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
