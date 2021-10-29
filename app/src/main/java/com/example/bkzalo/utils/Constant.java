bb542c51e6480b1c55133f28eaa3eefea260b14b
tuanit1 <dotuan9655@gmail.com>
2021-10-29 22:42:14 +0700

    cm

--- app/src/main/java/com/example/bkzalo/utils/Constant.java
+++ app/src/main/java/com/example/bkzalo/utils/Constant.java
@@ -1,6 +1,6 @@
 package com.example.bkzalo.utils;
 
 public class Constant {
-    public static String UID = "";
-    public static String SERVER_URL = "http://192.168.1.6/bkzalo/";
+    public static int UID = 1;
+    public static String SERVER_URL = "http://192.168.1.11/bkzalo/";
 }
